2e06e41f2d49f02b772bbfe7b8e453029e706ff8
pedrops164 <A95826@alunos.uminho.pt>
2022-04-14 22:17:39 +0100

    Simulador

---
+++ src/Simulador.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Simulador {
+    private Set<CasaInteligente> casasInteligentes;
+    private LocalDate data;
+
+    public Simulador() {
+        this.casasInteligentes = new TreeSet<>();
+        this.data = LocalDate.now();
+    }
+
+    public Simulador(LocalDate date) {
+        this.casasInteligentes = new TreeSet<>();
+        this.data = date;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
