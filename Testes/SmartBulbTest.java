15372f49dd0b049543736c9958729aeff96a6a24
72Rogan <danieldu109@gmail.com>
2022-05-17 08:10:54 +0100

    smartbulbtest

--- Testes/SmartBulbTest.java
+++ Testes/SmartBulbTest.java
@@ -1,9 +1,13 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static src.SmartDevice.Modo.OFF;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import src.Simulador;
+import src.SmartBulb;
 
 
 public class SmartBulbTest{
@@ -15,19 +19,17 @@ public class SmartBulbTest{
     @AfterEach
     public void tearDown(){}
 
-
-    Simulador simulador = new Simulador();
-
     @Test
     public void testeConstrutor(){
-        SmartBulb SmartBulb1 = new SmartBulb();
-        assertTrue(SmartBulb1!=null, "Erro ao criar a SmartBulb");
-        SmartBulb SmartBulb1 = new SmartBulb(simulador,"SmartBulb1",0);
-        assertTrue(SmartBulb1!=null, "Erro ao criar a SmartBulb");
-        SmartBulb SmartBulb1 = new SmartBulb(simulador,"SmartBulb1",5,OFF,0,15);
-        assertTrue(SmartBulb1!=null, "Erro ao criar a SmartBulb");
-        SmartBulb SmartBulb2 = new SmartBulb(SmartBulb1);
-        assertTrue(SmartBulb2!=null, "Erro ao criar a SmartBulb");
+        Simulador simulador = new Simulador();
+        SmartBulb smartBulb1 = new SmartBulb();
+        assertTrue(smartBulb1!=null, "Erro ao criar a SmartBulb");
+        smartBulb1 = new SmartBulb(simulador,"SmartBulb1",0);
+        assertTrue(smartBulb1!=null, "Erro ao criar a SmartBulb");
+        smartBulb1 = new SmartBulb(simulador,"SmartBulb1",OFF,0,8);
+        assertTrue(smartBulb1!=null, "Erro ao criar a SmartBulb");
+        smartBulb1 = new SmartBulb(smartBulb1);
+        assertTrue(smartBulb1!=null, "Erro ao criar a SmartBulb");
     }
     /*
     public void testConsumoDiario() {
@@ -44,12 +46,12 @@ public class SmartBulbTest{
 
     @Test
     public void testSetTone(){
-        SmartBulb SmartBulb1 = new SmartBulb();
-        SmartBulb1.setTone(5);
-        assertEquals(2,SmartBulb1.getTone(), "Valor da tonalidade não é o esperado");
-        SmartBulb SmartBulb1 = new SmartBulb();
-        SmartBulb1.setTone(-1);
-        assertEquals(0,SmartBulb1.getTone(), "Valor da tonalidade não é o esperado");
+        SmartBulb smartBulb1 = new SmartBulb();
+        smartBulb1.setTone(5);
+        assertEquals(2,smartBulb1.getTone(), "Valor da tonalidade não é o esperado");
+        smartBulb1 = new SmartBulb();
+        smartBulb1.setTone(-1);
+        assertEquals(0,smartBulb1.getTone(), "Valor da tonalidade não é o esperado");
     }
 
     @Test
@@ -62,25 +64,27 @@ public class SmartBulbTest{
 
     @Test
     public void testGetTone() {
-        SmartBulb SmartBulb1 = new SmartBulb();
-        assertEquals(1,SmartBulb1.getTone(), "Valor da tonalidade não é o esperado");
-        SmartBulb SmartBulb1 = new SmartBulb(simulador,"SmartBulb1",2);
-        assertEquals(2,SmartBulb1.getTone(), "Valor da tonalidade não é o esperado");
-        SmartBulb SmartBulb1 = new SmartBulb(simulador,"SmartBulb1",5,OFF,0,15);
-        assertEquals(0,SmartBulb1.getTone(), "Valor da tonalidade não é o esperado");
-        SmartBulb SmartBulb2 = new SmartBulb(SmartBulb1);
-        assertEquals(0,SmartBulb2.getTone(), "Valor da tonalidade não é o esperado");
+        Simulador simulador = new Simulador();
+        SmartBulb smartBulb1 = new SmartBulb();
+        assertEquals(1,smartBulb1.getTone(), "Valor da tonalidade não é o esperado");
+        smartBulb1 = new SmartBulb(simulador,"SmartBulb1",2);
+        assertEquals(2,smartBulb1.getTone(), "Valor da tonalidade não é o esperado");
+        smartBulb1 = new SmartBulb(simulador,"SmartBulb1",OFF,0,15);
+        assertEquals(0,smartBulb1.getTone(), "Valor da tonalidade não é o esperado");
+        SmartBulb smartBulb2 = new SmartBulb(smartBulb1);
+        assertEquals(0,smartBulb2.getTone(), "Valor da tonalidade não é o esperado");
     }
 
     @Test
     public void testGetTamanho() {
+        Simulador simulador = new Simulador();
  //       SmartBulb SmartBulb1 = new SmartBulb();
  //       assertEquals(1,SmartBulb1.getTone());
  //       SmartBulb SmartBulb1 = new SmartBulb(simulador,"SmartBulb1",2);
  //       assertEquals(2,SmartBulb1.getTone());
-        SmartBulb SmartBulb1 = new SmartBulb(simulador,"SmartBulb1",5,OFF,0,15);
-        assertEquals(15,SmartBulb1.getTamanho(),  "Valor do tamanho não é o esperado");
-        SmartBulb SmartBulb2 = new SmartBulb(SmartBulb1);
+        SmartBulb smartBulb1 = new SmartBulb(simulador,"SmartBulb1",OFF,0,15);
+        assertEquals(15,smartBulb1.getTamanho(),  "Valor do tamanho não é o esperado");
+        SmartBulb SmartBulb2 = new SmartBulb(smartBulb1);
         assertEquals(15,SmartBulb2.getTone(),  "Valor do tamanho não é o esperado");
     }
     
